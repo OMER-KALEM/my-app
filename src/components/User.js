f03f2de92cf4ee778af40cf56f7a36e1868ebbfa
Ömer KALEM <omer.kalem@ogr.sakarya.edu.tr>
2021-08-12 23:44:44 +0300

    User Arayuzu ve fontawesome

--- src/components/User.js
+++ src/components/User.js
@@ -17,13 +17,28 @@ class User extends Component {
         const {name,department,salary} = this.props;
 
         return (
+            /*Cirkin gozukuyor o yuzden duzenliycem*/ 
+            /*
             <div>
                 <ul>
-                    <li>Isim: {name}</li>
+                    <li>Isim: {name} <i className="far fa-trash-alt"></i> </li>
                     <li>Maas: {salary}</li>
                     <li>Departman: {department}</li>
                 </ul>
             </div>
+            */
+           <div className="col-md-8 mb-4">
+               <div className="card">
+                   <div className="card-header d-flex justify-content-between">
+                       <h4 className= "d-inline">{name}</h4>
+                       <i className="far fa-trash-alt" style= {{cursor : "pointer"}}></i>
+                   </div>
+                    <div className="card-body">
+                        <p className= "card-text"> Maas : {salary}</p>
+                        <p className= "card-text"> Department : {department}</p>
+                    </div>
+               </div>
+           </div>
         )
     }
 }
