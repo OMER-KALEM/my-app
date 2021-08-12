3a5cd74cc0a88eb91fd6eb405422e88ac56e1f0b
Ömer KALEM <omer.kalem@ogr.sakarya.edu.tr>
2021-08-12 20:07:40 +0300

    Project_my-app

---
+++ src/components/Navbar.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div>
+            <h3>User App</h3>
+        </div>
+    )
+}
+export default Navbar;
+
+
+
+
+
+
+
+/*
+import React from "react";
+
+const Navbar = () => {
+// function Navbar() {
+
+    return (
+        <div>
+
+            <h3>User App</h3>
+        </div>
+    )
+}
+
+export default Navbar;
+
+*/
