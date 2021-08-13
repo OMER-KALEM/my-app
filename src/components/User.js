3581e9a6be06281e51fe1c658e3f3ac1f95beef3
Ömer KALEM <omer.kalem@ogr.sakarya.edu.tr>
2021-08-13 10:09:29 +0300

    this.onClickEvent.bind

--- src/components/User.js
+++ src/components/User.js
@@ -3,13 +3,13 @@ import PropTypes from 'prop-types'
 
 
 class User extends Component {
-    /*
-    //User.defaultProps ile ayni islevde
+
     static defaultProps = {
         name : "Bilgi Yok",
         salary : "Bilgi yok",
         department : "Bilgi yok"
-    }*/
+    }
+
     constructor(props){
         super(props);
 
@@ -17,40 +17,38 @@ class User extends Component {
             test : "Test",
             isvisible: true
         }
+        //this.onClickEvent = this.onClickEvent.bind(this);
+    }
+
+//Arrow function ile normal function arasındaki fark ;
+//Arrow function "bind" islemini otomatik kendisi yapıyor    
+    onClickEvent = (number, e) => {
+        //console.log(e.target);
+        //console.log("test");
+        console.log(this);
+        console.log(number);
     }
 
     render() {
 
-        //Destructing
-
         const {name,department,salary} = this.props;
-
         const {isvisible} = this.state;
 
         return (
-            /*Cirkin gozukuyor o yuzden duzenliycem*/ 
-            /*
-            <div>
-                <ul>
-                    <li>Isim: {name} <i className="far fa-trash-alt"></i> </li>
-                    <li>Maas: {salary}</li>
-                    <li>Departman: {department}</li>
-                </ul>
-            </div>
-            */
            <div className="col-md-8 mb-4">
                <div className="card">
                    <div className="card-header d-flex justify-content-between">
-                       <h4 className= "d-inline">{name}</h4>
+                       <h4 className= "d-inline" onClick = {this.onClickEvent.bind(this, 34)}>{name}</h4>
                        <i className="far fa-trash-alt" style= {{cursor : "pointer"}}></i>
                    </div>
 
                     {
-                        isvisible ? <div className="card-body">
+                        isvisible ? 
+                        <div className="card-body">
                         
-                        <p className= "card-text"> Maas : {salary}</p>
-                        <p className= "card-text"> Department : {department}</p>
-                        <p>{this.state.test}</p>
+                            <p className= "card-text"> Maas : {salary}</p>
+                            <p className= "card-text"> Department : {department}</p>
+                            <p>{this.state.test}</p>
                     
                         </div> : null
                     }
@@ -59,12 +57,14 @@ class User extends Component {
         )
     }
 }
+/*
 User.defaultProps = {
     name : "Bilgi Yok",
     salary : "Bilgi yok",
     department : "Bilgi yok"
 
 }
+*/
 
 User.propTypes = {
     name : PropTypes.string.isRequired,
