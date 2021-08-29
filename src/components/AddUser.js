52a995dcc92d4737a2235aafdb88df2ba9ba8ee5
Ömer KALEM <omer.kalem@ogr.sakarya.edu.tr>
2021-08-29 18:55:36 +0300

    React Pose Animation

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -1,9 +1,40 @@
 import React, { Component } from 'react'
+import posed from 'react-pose';
 
+const HideAndShowAnimation = posed.div({
+    visible: { 
+        opacity: 1,
+        applyAtStart: {
+            display : "block"
+        }
+    },
+
+    hidden: {
+        opacity: 0,
+        applyAtEnd: {
+            display : "none"
+        }
+    }
+  });
+
+  
 class AddUser extends Component {
+    state = {
+        visible : false
+    }
+
+    changeVisibilty = (e) => {
+        this.setState({
+            visible : !this.state.visible
+        })
+    }
+
     render() {
+        const {visible} = this.state;
         return (
             <div className="col-md-8 mb-4">
+                <button onClick={this.changeVisibilty} className="btn btn-dark btn-block mb-2">{visible ? "Hide Form" : "Show Form"}</button>
+                <HideAndShowAnimation pose = {visible ? "visible" : "hidden"}>
                 <div className="card">
                     <div className="card-header">
                         <h4>Add User Form</h4>
@@ -17,7 +48,7 @@ class AddUser extends Component {
                                 name = "name"
                                 id = "id"
                                 placeholder = "Enter Name"
-                                class = "form-control"
+                                className = "form-control"
                                 />
                             </div>
                             <div className="form-group">
@@ -27,7 +58,7 @@ class AddUser extends Component {
                                 name = "department"
                                 id = "department"
                                 placeholder = "Enter Department"
-                                class = "form-control"
+                                className = "form-control"
                                 />
                             </div>
                             <div className="form-group">
@@ -37,14 +68,14 @@ class AddUser extends Component {
                                 name = "salary"
                                 id = "salary"
                                 placeholder = "Enter Salary"
-                                class = "form-control"
+                                className = "form-control"
                                 />
                             </div>
-                            <button class="btn btn-danger btn-block" type="submit">Add User</button>
+                            <button className ="btn btn-danger btn-block" type="submit">Add User</button>
                         </form>
                     </div>
                 </div>
-                
+                </HideAndShowAnimation>
             </div>
         )
     }
